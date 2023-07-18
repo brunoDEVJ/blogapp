3ca93283da1b5c3d187553c246cac73950afaf12
Bruno <109311971+brunoDEVJ@users.noreply.github.com>
2023-07-18 09:51:34 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const getMongoURL = () => {
   if (process.env.NODE_ENV === 'production') {
-    return "mongodb+srv://boylima22:V7NjZEfwS4hhnRJ4@blogapp.cihpuji.mongodb.net/";
+    return "mongodb+srv://brunodevj:0EYYa1UCNrIXLUd0@blogapp.mrz5sbd.mongodb.net/?retryWrites=true&w=majori";
   }
   return "mongodb://127.0.0.1/blogapp";
 };
